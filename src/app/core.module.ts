a5d4df6ef9762a042e5fd5b79201c443176c5040
Selaml <selamawitlb@@gmail.com>
2026-01-22 11:26:10 +0300

    add team on core module

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PlayersModule } from './modules/players/players.module';
+import { TeamsModule } from './modules/teams/teams.module';
 
 @Global()
 @Module({
   imports: [
-    PlayersModule
+    PlayersModule,
+    TeamsModule
   ],
 })
 export class CoreModule { }
